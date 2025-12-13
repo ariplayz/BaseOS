ca2c1c416040a0e7808acd6bdb02fb7f7a2fcb79
Ari Cummings <ari@aricummings.com>
2025-12-12 17:30:04 -0800

    Fix

--- build/CMakeFiles/cppefi.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cppefi"
-  "cppefi.efi"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cppefi.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- main.cpp
+++ main.cpp
@@ -1,25 +1,16 @@
+extern "C" {
 
 #include <efi.h>
 #include <efilib.h>
-#include "UEFIConsole.h"
-#include "Login.h"
 
-static const CHAR16* hostname = L"BaseOS";
-static const CHAR16* version = L"0.1-alpha";
-
-void runApp(EFI_SYSTEM_TABLE* system_table) {
-    UEFIConsole console(system_table);
-
-    console.print(L"Welcome to BaseOS!\n");
-    console.printf(L"Version: %s\n", version);
-
-    Login login(console);
-    login.start();
 }
 
+
 extern "C"
-EFI_STATUS EFIAPI efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table) {
+EFI_STATUS EFIAPI
+efi_main (EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table)
+{
     InitializeLib(image_handle, system_table);
-    runApp(system_table);
+    Print((CHAR16*)L"Hello from the UEFI\n");
     return EFI_SUCCESS;
 }
