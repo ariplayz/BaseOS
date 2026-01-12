3603599b88d152f66d4d494eb78ffc713789c6c4
Ari Cummings <ari@aricummings.com>
2026-01-12 11:44:34 -0800

    chmod

---
+++ build/CMakeFiles/cppefi.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for cppefi.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/main_obj.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/aric/CLionProjects/BaseOS/UefiCin.cpp" "CMakeFiles/main_obj.dir/UefiCin.cpp.o" "gcc" "CMakeFiles/main_obj.dir/UefiCin.cpp.o.d"
+  "/home/aric/CLionProjects/BaseOS/UefiConsole.cpp" "CMakeFiles/main_obj.dir/UefiConsole.cpp.o" "gcc" "CMakeFiles/main_obj.dir/UefiConsole.cpp.o.d"
+  "/home/aric/CLionProjects/BaseOS/main.cpp" "CMakeFiles/main_obj.dir/main.cpp.o" "gcc" "CMakeFiles/main_obj.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
