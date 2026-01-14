4ac304fa09c1d93bae94a869dc89ac0369f9cdc9
Ari Cummings <ari@aricummings.com>
2026-01-13 16:42:55 -0800

    Add config files

---
+++ build/CMakeFiles/main_obj.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_obj.dir/Console.cpp.o"
+  "CMakeFiles/main_obj.dir/Console.cpp.o.d"
+  "CMakeFiles/main_obj.dir/main.cpp.o"
+  "CMakeFiles/main_obj.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_obj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
